566ecb622e15a68c2103c68cece7b5cd62d46494
danyramirezg <danyramirezg@gmail.com>
2021-05-03 12:19:06 -0500

    Async is a builder like launch, except it returns a result

---
+++ app/src/main/java/com/techyourchance/coroutines/exampleAsync.kt
@@ -0,0 +1,39 @@
+package com.techyourchance.coroutines
+
+import android.util.Log
+import kotlinx.coroutines.async
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.runBlocking
+import kotlin.random.Random
+
+// Async is a builder like launch, except it returns a result.
+fun main() {
+
+    runBlocking {
+        val firstDeferred = async { getFirstValue() }
+        val secondDeferred = async { getSecondValue() }
+
+        println("Doing some processing here")
+        delay(500L)
+        println("Waiting for values...")
+
+        val firstValue = firstDeferred.await()
+        val secondValue = secondDeferred.await()
+    }
+
+}
+
+
+suspend fun getFirstValue(): Int {
+    delay(1000L)
+    val value = Random.nextInt(100)
+    println("Returning first value $value")
+    return value
+}
+
+suspend fun getSecondValue(): Int {
+    delay(2000L)
+    val value = Random.nextInt(101, 200)
+    print("Returning second value $value")
+    return value
+}
